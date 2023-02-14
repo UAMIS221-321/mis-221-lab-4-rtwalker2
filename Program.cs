38ab11eee9f360a1f0e89f8897c3c86a3da6b571
rtwalker2 <123768967+rtwalker2@users.noreply.github.com>
2023-02-13 19:51:25 -0600

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿using System;
 
+GetFullTriangle();
+
+
+
+
+static void DisplayMenu() {
+    Console.Clear();
+    Console.WriteLine("Enter 1 to display full triangle\nEnter 2 to display partial triangle\nEnter 3 to exit");
+}
+
+static void GetFullTriangle() {
+    Random rand = new Random();
+    int rowNum = rand.Next(3,10);
+    
+    for(int row = 1; row <= rowNum; row++) {
+        for(int col = 1; col <= row; col++) {
+            Console.Write("0");
+        }
+        Console.WriteLine();
+    }
+}
+
+static void GetPartialTriangle() {
+    
+}
